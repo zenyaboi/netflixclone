b913d52dbf6e6e4583e320c7da7428ed664aabf6
zenyaboi <bannedboyinlife@gmail.com>
2022-08-03 14:38:07 -0300

    fetched tmdb api

---
+++ components/Banner.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+
+function Banner() {
+  return (
+    <div>
+      {/* <div>
+        <Image src=""/>
+      </div> */}
+    </div>
+  )
+}
+
+export default Banner
